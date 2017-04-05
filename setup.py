54183668fec3cb6a03ece09e03788fe8fa7fbef6
Shuhei Iitsuka <tushuhei@google.com>
2017-04-05 15:51:54 +0900

    Fix punctuation processing to consider the direction.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='budou',
-    version='0.2.2',
+    version='0.2.3',
     author='Shuhei Iitsuka',
     author_email='tushuhei@google.com',
     description='CJK Line Break Organizer',
