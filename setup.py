5b51796fc72800b022f6097895a78e2f49af2cf1
Shuhei Iitsuka <tushuhei@google.com>
2017-08-15 12:02:51 +0900

    0.3.2 release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='budou',
-    version='0.3.1',
+    version='0.3.2',
     author='Shuhei Iitsuka',
     author_email='tushuhei@google.com',
     description='CJK Line Break Organizer',
