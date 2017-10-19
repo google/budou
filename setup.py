fae029a35e380a9a0f5f95e5026554ecc88433e1
Shuhei Iitsuka <tushuhei@google.com>
2017-10-19 16:33:17 +0900

    0.7.5+html5lib-1.0b8 release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_file(name):
 
 setup(
     name='budou',
-    version='0.7.4+html5lib-1.0b8',
+    version='0.7.5+html5lib-1.0b8',
     author='Shuhei Iitsuka',
     author_email='tushuhei@google.com',
     description='CJK Line Break Organizer',
