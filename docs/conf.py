6ed3761fbfb2a63c1890d7a759023059f724e9da
Shuhei Iitsuka <tushuhei@google.com>
2018-09-03 13:24:51 +0900

    Update module path in conf.py to activate autodoc.

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, '/Users/tushuhei/Projects/budou/budou')
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.abspath('../..'), 'budou'))
 
 
 # -- Project information -----------------------------------------------------
